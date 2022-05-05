0e5d88905e635c6426aa1d80f158d7700254d23f
Olesia Yurchenko <olesya.yurchenko91@gmail.com>
2022-05-06 01:09:30 +0300

    Updates

---
+++ static/js/696.2e4b669e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[696],{696:function(e,t,i){i.r(t);var n=i(501),r=i(184);t.default=function(){return(0,r.jsxs)("nav",{children:[(0,r.jsx)(n.OL,{end:!0,to:"/",style:function(e){return{marginRight:10,color:e.isActive?"red":"black"}},children:"Home"}),(0,r.jsx)(n.OL,{to:"/movies",style:function(e){return{color:e.isActive?"red":"black"}},children:"Movies"})]})}}}]);
+//# sourceMappingURL=696.2e4b669e.chunk.js.map
